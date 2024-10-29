02d687109342687ba11c48859d3b6235f77d4202
Marcelo <marceloxdnn@gmail.com>
2024-10-29 05:24:29 -0500

    cambios

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Mundo!\n');
+  res.send('Lo di todo :(\n');
 });
 
 app.get('/mars', function(req, res) {
